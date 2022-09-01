4e3754315daf6e9dfa7e6d2f6a03711511d70126
Jaison Machado <jaisonmachado.ans@gmail.com>
2022-09-01 16:27:15 -0300

    Desafio concluido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using static System.Console;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -16,32 +18,50 @@ namespace DesafioFundamentos.Models
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            bool pronto =false;
+            while (!(pronto))
+            {
+                Console.WriteLine("Digite a placa do veículo para estacionar:");
+                string placa=ReadLine().Trim();
+                placa = placa.ToUpper();
+                if (string.IsNullOrEmpty(placa)) WriteLine("O campo placa não pode estar vazio!");
+                else 
+                {
+                    veiculos.Add(placa);
+                    pronto = true;
+                 }
+            }            
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            WriteLine("Digite a placa do veículo para saída:");
+            string placa=ReadLine().Trim();
+            placa=placa.ToUpper();
+            bool pronto = false;
+            while (!(pronto))
+            { 
+                if (string.IsNullOrEmpty(placa)) WriteLine("O campo placa não pode estar vazio!");
+                else pronto = true;
+                
+            }
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos.Any(x => x.ToUpper() == placa))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
+                int horas = 0;
+                WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                horas = int.Parse(ReadLine());
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
+                
                 decimal valorTotal = 0; 
-
+                valorTotal= precoInicial + (precoPorHora * horas);
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal.ToString("0.00")}");
             }
             else
             {
@@ -57,6 +77,12 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                
+                foreach(var veiculo in veiculos)
+                {
+                        WriteLine(veiculo);
+                }
+                
             }
             else
             {
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -44,6 +44,7 @@ while (exibirMenu)
             break;
 
         case "4":
+            
             exibirMenu = false;
             break;
 
@@ -55,5 +56,5 @@ while (exibirMenu)
     Console.WriteLine("Pressione uma tecla para continuar");
     Console.ReadLine();
 }
-
+Console.WriteLine("Obrigado por usar DIO.Estacionamento!");
 Console.WriteLine("O programa se encerrou");
